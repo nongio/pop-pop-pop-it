6939975077e981d29f289799d402ba08b6b39316
Riccardo Canalicchio <riccardo.canalicchio@zalando.de>
2018-12-07 11:38:30 +0100

    baloon view

--- screens/PlayScreen.js
+++ screens/PlayScreen.js
@@ -8,22 +8,92 @@ import {
     TouchableWithoutFeedback,
     View,
     Animated,
+    Alert,
+    Vibration,
 } from 'react-native';
 
-export default class PlayScreen extends React.Component {
+const DURATION = 200
 
-  render() {
-    return (
-      <View style={styles.container}>
-      </View>
-    );
+
+
+export default class PlayScreen extends React.Component {
+    constructor () {
+        super()
+        this.state = {
+            size: 0.1,
+        };
+
+        this.balloonScale= new Animated.Value(this.state.size);
+
+    }
+    onPress = () => {
+        if(this.state.size > 1.0) {
+            this.setState({
+                size: 0
+            });
+            Animated.timing(
+                this.balloonScale,
+                {
+                    toValue:  0,
+                    duration: 0,
+                }
+            ).start();
+            Vibration.vibrate(DURATION)
+            Alert.alert(
+                'Boooom!',
+                '',
+                [
+                    {text: 'OK', onPress: () => console.log('OK Pressed')},
+                ],
+            );
+
+        } else {
+            this.setState({
+                size: this.state.size + 0.1
+            });
+            Animated.spring(
+                this.balloonScale,
+                {
+                toValue:  this.state.size,
+                friction: 1
+                }
+            ).start()
+
+        }
+    }
+
+    render() {
+        return (
+        <View style={styles.container}>
+            <Animated.Image
+                style={{ width: 341, height: 443, transform: [{scale: this.balloonScale}] }}
+                source={require('../assets/images/balloon.png')}/>
+
+                <TouchableWithoutFeedback style={styles.button} onPress={this.onPress}>
+                    <View style={styles.buttonInner}></View>
+                </TouchableWithoutFeedback>
+        </View>
+        );
   }
 
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    button: {
+        marginTop: 100,
+        width: 100,
+        height: 100,
+    },
+    buttonInner: {
+        width: 150,
+        height: 150,
+        backgroundColor: '#eb0045',
+        borderRadius: 75,
+    },
 });
